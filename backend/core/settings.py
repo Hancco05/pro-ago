70e92282ef085c9c0926d9260c83edfd59962ed6
Sebastian Ignacio Carmona Wright <s.carmonawright@gmail.com>
2026-08-06 12:06:52 -0400

    com12

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,18 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',           # <-- Nuevo
+    'rest_framework',
     'corsheaders', 
     'gestion_tareas',
     'info_rrhh',
-    CORS_ALLOWED_ORIGINS = 
-    "http://localhost:5173",
 ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    CORS_ALLOWED_ORIGINS = 
-    "http://localhost:5173",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
