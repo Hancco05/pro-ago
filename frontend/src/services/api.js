49a27a1e95cd461e9f5608271c6a0f780cd36ca9
Sebastian Ignacio Carmona Wright <s.carmonawright@gmail.com>
2026-08-24 16:49:24 -0400

    com42

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,14 +8,15 @@ const api = axios.create({
 });
 
 // 🔑 Interceptor para añadir el token a cada petición
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, error => {
-  return Promise.reject(error);
-});
+api.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
 
 export default api;
